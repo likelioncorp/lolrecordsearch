bef0c4476da3fa595f64543b0a2f2a42bc30ea76
byoth <taehyunjo97@gmail.com>
2015-01-07 14:15:31 +0900

    [검색 페이지 만들기] 검색 결과 페이지로 데이터 전송하기
    current:
    <input> 태그에서 키보드를 눌렀을 때 그 키의 keyCode가 13(ENTER)일 경우 <button>을 클릭했을 때와 같은 이벤트가 발생하도록 해주세요.
    
    after:
    잘했어요.
    
    ToyId: fbc107d56e6fb34b7f69c509ca0c295d5cd8ab91

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     var name = $('#SearchForm input').val();
     location.href = '/summoner/' + name;
   });
+  $('#SearchForm input').keydown(function(event) {
+    if(event.keyCode == 13) {
+      $('#SearchForm button').click();
+    }
+  });
 });
